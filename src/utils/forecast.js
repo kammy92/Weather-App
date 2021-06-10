a11f0ef16536e6ff82637cc0ebed8f91d9634dd0
Karman Singh <karman.channa@aricent.com>
2021-06-10 22:37:52 +0530

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,14 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=1a0c791ca8ab3ae094fc8630384545a0&query=" + latitude + "," + longitude
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to weather service!", undefined)
+        } else if (body.error) {
+            callback("Unable to find location. Try another search.", undefined)
+        } else {
+            callback(undefined, "It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+        }
+    })
+}
+module.exports = forecast
